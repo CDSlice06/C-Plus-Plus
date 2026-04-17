d4a75563ab6662431b03b72af9fb5147b72d83f9
finger-king <3413823923@qq.com>
2026-04-17 23:29:27 +0800

    究极优化

--- 4_17_01/4_17_01/4_17_01.cpp
+++ 4_17_01/4_17_01/4_17_01.cpp
@@ -50,3 +50,24 @@ public:
     }
 };
 
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0, n = nums.size();
+        for (int i = n - 1; i >= 2; i--)
+        {
+            int left = 0, right = i - 1;
+            while (left < right)
+            {
+                if (nums[left] + nums[right] > nums[i])
+                {
+                    sum += right - left;
+                    right--;
+                }
+                else left++;
+            }
+        }
+        return sum;
+    }
+};
